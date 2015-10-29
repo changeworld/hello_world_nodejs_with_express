72a47b571171ef2f0d29689ca2f19f0c83542789
changeworld <changesworlds@gmail.com>
2015-10-29 16:08:18 +0900

    Add gulp-istanbul

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
 var mocha = require('gulp-mocha');
+var istanbul = require('gulp-istanbul');
 
 gulp.task('test', function() {
   return gulp.src('./test/**/*.js')
     .pipe(mocha({reporter: 'mocha-junit-reporter'}));
+});
+
+gulp.task('cover', function() {
+  return gulp.src('./test/**/*.js')
+    .pipe(mocha())
+    .pipe(istanbul.writeReports({reporters: [ 'lcov', 'json', 'text', 'text-summary', 'cobertura']}))
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
 });
